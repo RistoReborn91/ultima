425dd40d79bfeaf3b7fe7459f6459583edfbae73
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-03-04 20:08:13 -0500

    Update autoicon.js

--- autoicon.js
+++ autoicon.js
@@ -1,13 +1,13 @@
-function updateIcon(e) {
+function updateIcon() {
 
-// var e = entry();
+var e = entry();
 var uri = e.field("IconJS");
 
 
 uri = uri.replace("[", "");
 uri = uri.replace("]", "");
 
-  e.set("Icon", uri);
+e.set("Icon", uri);
 
 message("Icon updated");
   
