7b62b996fbfd2d0da95c657d4565cca049a88a4c
RistoReborn91 <chris.lee.designer@gmail.com>
2021-03-24 16:52:05 -0400

    Update countActiveLinks.js

--- countActiveLinks.js
+++ countActiveLinks.js
@@ -1,29 +1,31 @@
 function countActiveLinks(e) {
-var tasklib = libByName("𝗔𝟰160    Assignments");
-var links = tasklib.linksTo(e);
-var counter = "Active Links";
-var total = links.length;
-var numActive = 0;
+	var tasklib = libByName("𝗔𝟰160    Assignments");
+	var links = tasklib.linksTo(e);
+	var counter = "Active Links";
+	var total = links.length;
+	var numActive = 0;
 
-var maxPriorityLevel = 0;
+	var maxPriorityLevel = 0;
 
-for (var i = 0; i < total; i++) {
+	for (var i = 0; i < total; i++) {
 
-  var status = links[i].field("✔ Status");
-  var priority = links[i].field("Priority");
+	  var status = links[i].field("✔ Status");
+	  var priority = links[i].field("Priority");
 
-  if (status == "⑤  Incomplete" || status == "⑥  Ongoing")
-    numActive++
+	  if (status == "⑤  Incomplete" || status == "⑥  Ongoing")
+		numActive++
 
-  if (priority > maxPriorityLevel)
-	maxPriorityLevel = priority;
-  
-}
+	  if (priority > maxPriorityLevel)
+		maxPriorityLevel = priority;
+	  
+	}
 
-e.set("Active Links", numActive);
-e.set("Total Links", total);
-e.set("Max Priority Level", maxPriorityLevel);
+	e.set("Active Links", numActive);
+	e.set("Total Links", total);
+	e.set("Max Priority Level", maxPriorityLevel);
+	
+	message(maxPriorityLevel " + "MPL");
 
-// message(numActive + " active links");
+	// message(numActive + " active links");
 
 }
